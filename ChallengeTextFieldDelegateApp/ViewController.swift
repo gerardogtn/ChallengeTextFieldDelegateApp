8edfcf2e67011ea6e817bebb9de5f15678f66038
Gerardo Teruel <gerardogtn@gmail.com>
2016-05-31 13:23:34 -0500

    :rocket: Check that user input is only numbers and that the maximum length is 5 digits

--- ChallengeTextFieldDelegateApp/ViewController.swift
+++ ChallengeTextFieldDelegateApp/ViewController.swift
@@ -22,6 +22,7 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    zipCodeTextField.delegate = zipCodeDelegate
     lockedTextField.delegate = lockDelegate
   }
 
